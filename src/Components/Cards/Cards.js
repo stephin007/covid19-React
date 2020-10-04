b5554941463bea401efcd948b5ef726f541f9cb4
stephin007 <stephinreji123@gmail.com>
2020-10-04 12:15:06 +0530

    Added Styles to the cards component

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Card, CardContent, Typography, Grid} from '@material-ui/core'
 import CountUp from 'react-countup'
+import cx from 'classnames'
 
 import styles from './Cards.module.css'
 
@@ -25,7 +26,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) =>{
                 spacing={3}
                 justify="center"
             >
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12}md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Infected
@@ -46,7 +47,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) =>{
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12}md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Recovered
@@ -67,7 +68,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) =>{
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12}md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Deaths
