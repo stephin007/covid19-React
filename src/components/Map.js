2d2df2cb8ae88963b657a140a2be7cbd58713471
stephin007 <stephinreji123@gmail.com>
2020-10-18 02:16:41 +0530

    rendered the leaflet map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {Map as LeafletMap, TileLayer} from 'react-leaflet'
+import '../componentCSS/Map.css'
 
-function Map() {
+
+function Map({center, zoom}) {
     return (
         <div className="map">
-            <h1>I am A map</h1>
+            <LeafletMap center={center}zoom={zoom}>
+                <TileLayer 
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </LeafletMap>
         </div>
     )
 }
