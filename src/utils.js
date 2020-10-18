82a9948b9dc0eb8a64eaa37d7b67bdcdb45f980b
stephin007 <stephinreji123@gmail.com>
2020-10-18 09:51:57 +0530

    Added function for making circles

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,22 @@
+import React from 'react'
+import numeral from 'numeral'
+import {Circle, Popup} from 'react-leaflet'
+
+const casesTypeColors = {
+    cases: {
+      hex: "#CC1034",
+      multiplier: 800
+    },
+    recovered: {
+      hex: "#7dd71d",
+      multiplier: 1200
+    },
+    deaths: {
+      hex: "#fb4443",
+      multiplier: 2000
+    },
+  };
+
 export const sortData = (data) =>{
     const sortedData = [...data];
 
@@ -10,3 +29,28 @@ export const sortData = (data) =>{
     })
     return sortedData
 }
+
+// Draw circles
+export const  showDataOnMap = (data, casesType='cases')=>(
+    data.map(country =>(
+        <Circle
+            center={[country.countryInfo.lat, country.countryInfo.long]}
+            fillOpacity={0.4}
+            color={casesTypeColors[casesType].hex}
+            fillcolor={casesTypeColors[casesType].hex}
+            radius={
+                Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+            }
+        >
+            <Popup>
+                <div>
+                    <div style={{backgroundImage: `URL(${country.countryInfo.flag})`}}/>
+                    <div>{country.country}</div>
+                    <div>Cases: {numeral(country.cases).format('0,0')}</div>
+                    <div>Recovred: {numeral(country.recovered).format('0,0')}</div>
+                    <div>Deaths: {numeral(country.deaths).format('0,0')}</div>
+                </div>
+            </Popup>
+        </Circle>
+    ))
+)
